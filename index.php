91513d26f7ff7d29bfe0566a1095bc4ba9ee8412
Alexey Mazheika <mozh.by@gmail.com>
2015-08-17 11:30:00 +0300

    error not found 404 in result

--- index.php
+++ index.php
@@ -39,36 +39,39 @@ Callback:
 
 <script>
     $('#categoryrequestbutton').click(function(){
-        $('.list-group').append('<a href="#" class="list-group-item"><i class="fa fa-comment fa-fw"></i> API Call Process Started . <span class="pull-right text-muted small"><em></em></span> </a><br />');
-        $('.list-group').append('<a href="#" class="list-group-item"><i class="fa fa-comment fa-fw"></i> Getting Data from The filled FORM and converting to json. <span class="pull-right text-muted small"><em></em></span> </a><br />');
+        $('.list-group').append('<a href="#">API Call Process Started .</a><br />');
+        $('.list-group').append('<a href="#">Getting Data from The filled FORM and converting to json.</a><br />');
         var datastring = [];
         datastring = {"username":$('#categorydiv #username').val() , "password": $('#categorydiv #password').val()} ;
         var jsonstring = JSON.stringify(datastring);
-        $('.list-group').append('<a href="#" class="list-group-item"><i class="fa fa-comment fa-fw"></i> JSON String created from the data <br /> '+jsonstring+' <span class="pull-right text-muted small"><em></em></span> </a><br />');
-        $('.list-group').append('<a href="#" class="list-group-item"><i class="fa fa-comment fa-fw"></i> Encrypting the data using the private key for secure Transportation <br /> '+jsonstring+' <span class="pull-right text-muted small"><em></em></span> </a><br />');
+        $('.list-group').append('<a href="#">JSON String created from the data <br /> '+jsonstring+'</a><br />');
+        $('.list-group').append('<a href="#">Encrypting the data using the private key for secure Transportation <br /> '+jsonstring+'</a><br />');
         var encryptedstring = Aes.Ctr.encrypt(jsonstring,$('#categorydiv #privatetoken').val(), 256);
         var encodedtoken = Base64.encode($('#categorydiv #publictoken').val());
-        $('.list-group').append('<a href="#" class="list-group-item"><i class="fa fa-comment fa-fw"></i> Encrypted Above given json to the following string <br /> '+encryptedstring+' <span class="pull-right text-muted small"><em></em></span> </a><br />');
-        $('.list-group').append('<a href="#" class="list-group-item"><i class="fa fa-comment fa-fw"></i> Adding the Following Encrypted String to an argument that will be passed to the MConnect API<br /> postedinfo='+encryptedstring+' <span class="pull-right text-muted small"><em></em></span> </a><br />');
-        $('.list-group').append('<a href="#" class="list-group-item"><i class="fa fa-comment fa-fw"></i> Encoding the Public Token with Base64 Encoding so '+$('#categorydiv #publictoken').val()+' becomes as follows<br /> token='+encodedtoken+' <span class="pull-right text-muted small"><em></em></span> </a><br />');
-        $('.list-group').append('<a href="#" class="list-group-item"><i class="fa fa-comment fa-fw"></i> Adding the Base64 Encoded Public Token as an argument that will be passed to the MConnect API<br /> token='+encodedtoken+' <span class="pull-right text-muted small"><em></em></span> </a><br />');
-        $('.list-group').append('<a href="#" class="list-group-item"><i class="fa fa-comment fa-fw"></i> Compiling the Complete URL that will be passed to the MConnect API<br /> '+$('#posturl').val()+'?postedinfo='+encryptedstring+'&token='+encodedtoken+' <span class="pull-right text-muted small"><em></em></span> </a><br />');
-        $('.list-group').append('<a href="#" class="list-group-item"><i class="fa fa-comment fa-fw"></i> Making the POST Request to the API as given below <br /> '+$('#posturl').val()+'?postedinfo='+encryptedstring+'&token='+encodedtoken+' <span class="pull-right text-muted small"><em></em></span> </a><br />');
+        $('.list-group').append('<a href="#">Encrypted Above given json to the following string <br /> '+encryptedstring+'</a><br />');
+        $('.list-group').append('<a href="#">Adding the Following Encrypted String to an argument that will be passed to the MConnect API<br /> postedinfo='+encryptedstring+'</a><br />');
+        $('.list-group').append('<a href="#">Encoding the Public Token with Base64 Encoding so '+$('#categorydiv #publictoken').val()+' becomes as follows<br /> token='+encodedtoken+'</a><br />');
+        $('.list-group').append('<a href="#">Adding the Base64 Encoded Public Token as an argument that will be passed to the MConnect API<br /> token='+encodedtoken+'</a><br />');
+        $('.list-group').append('<a href="#">Compiling the Complete URL that will be passed to the MConnect API<br /> '+$('#posturl').val()+'?postedinfo='+encryptedstring+'&token='+encodedtoken+'</a><br />');
+        $('.list-group').append('<a href="#">Making the POST Request to the API as given below <br /> '+$('#posturl').val()+'?postedinfo='+encryptedstring+'&token='+encodedtoken+'</a><br />');
         $.ajax({
             async: false,
             type: 'POST',
             dataType:'jsonp',
             url: $('#posturl').val(),
             data:{postedinfo:encryptedstring,token:encodedtoken},
+
             success: function(data){
+                /*
                 if(data.success == false)
                 {
                     alert(data.error);
-                    $('.list-group').append('<a href="#" class="list-group-item"><i class="fa fa-comment fa-fw"></i> Received Error Message ( '+data.error+' ) from the API by posting the following information <br /> '+$('#posturl').val()+'?postedinfo='+encryptedstring+'&token='+$('#categorydiv #publictoken').val()+' <span class="pull-right text-muted small"><em></em></span> </a>');
+                    $('.list-group').append('<a href="#">Received Error Message ( '+data.error+' ) from the API by posting the following information <br /> '+$('#posturl').val()+'?postedinfo='+encryptedstring+'&token='+$('#categorydiv #publictoken').val()+'</a>');
                     $('.resultdiv').html(data.error)
                     return false;
                 }
-                $('.list-group').append('<a href="#" class="list-group-item"><i class="fa fa-comment fa-fw"></i> Received Result ( '+JSON.stringify(data)+' ) from the API by posting the following information <br /> '+$('#posturl').val()+'?postedinfo='+encryptedstring+'&token='+$('#categorydiv #publictoken').val()+' <span class="pull-right text-muted small"><em></em></span> </a>');
+                alert('tut');
+                $('.list-group').append('<a href="#">Received Result ( '+JSON.stringify(data)+' ) from the API by posting the following information <br /> '+$('#posturl').val()+'?postedinfo='+encryptedstring+'&token='+$('#categorydiv #publictoken').val()+'</a>');
                 var optionstr = '<option value="">Select Category</option>';
                 for(var i=0; i<data.length; i++)
                 {
@@ -81,13 +84,13 @@ Callback:
 
                 $('#brandsdiv #categories').append(optionstr);
                 $('#denomdiv #categories').append(optionstr);
-                $('#voucherdiv #vouchercategories').append(optionstr);
+                $('#voucherdiv #vouchercategories').append(optionstr);*/
                 $('.resultdiv').html(JSON.stringify(data));
             },
-            error: function(jqXHR,textStatus, errorThrown){
+            error: function(jqXHR, textStatus, errorThrown){
                 var error = jqXHR.responseJSON.error
                 alert(error);
-                $('.list-group').append('<a href="#" class="list-group-item"><i class="fa fa-comment fa-fw"></i> Received Error Message ( '+JSON.stringify(jqXHR.responseJSON)+' ) from the API by posting the following information <br /> '+$('#posturl').val()+'?postedinfo='+encryptedstring+'&token='+encodedtoken+' <span class="pull-right text-muted small"><em></em></span> </a>');
+                $('.list-group').append('<a href="#">Received Error Message ( '+JSON.stringify(jqXHR.responseJSON)+' ) from the API by posting the following information <br /> '+$('#posturl').val()+'?postedinfo='+encryptedstring+'&token='+encodedtoken+'</a>');
                 $('.resultdiv').html(error)
                 return false;
             }
